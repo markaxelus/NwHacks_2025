1689eb5aa6b923d96571628e43fc005cbcad55ea
Mark <mrkaxelus@gmail.com>
2025-01-18 14:47:00 -0800

    Home Page, navigation not setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import  Navbar  from './components/Navbar'
 
 
-import Home from './pages/Home'
+import Home from './pages/Home' 
 
 
 import './styles/global.css'
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import Logo from "../assets/MindMap.png";
+import ArrowDown from "../assets/arrow-down.png";
+import { useNavigate } from "react-router-dom";
+
 
 const Home = () => {
-  return (
-    <div>
-        <div>
-            
-        </div>
-    </div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Home
+  return (
+    <div
+      id="home"
+      className="flex justify-center items-center pb-[2rem] h-[110vh] flex-col space-y-12 text-center"
+    >
+      <h1 className="font-bold text-4xl">Welcome to Link</h1>
+      <img src={Logo} alt="logo" className="w-[100px] h-[100px]" />
+
+      <div className="font-bold">
+        <p>
+          Discover a faster, clearer way to learn! Upload PDFs and watch them <br />
+          transform into easy-to-understand diagrams designed for visual thinkers
+        </p>
+      </div>
+
+      <button
+        className="border-2 border-black px-14 py-2 rounded-lg text-white bg-primary"
+        onClick={() => navigate("")}
+      >
+        Generate a Diagram
+      </button>
+
+      <div className="pt-12">
+        <a href="">
+          <button className="border-2 border-black p-3 rounded-full">
+            <img
+              src={ArrowDown}
+              alt="arrow-down"
+              className="w-6 h-6 transition-transform duration-300 ease-in-out transform hover:scale-60 hover:translate-y-1"
+            />
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
