a4710983eddc0d542d6f9bba0d6ac14c65acb689
Mark <mrkaxelus@gmail.com>
2025-01-18 14:14:51 -0800

    Navbar component, no styling for left navi

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,21 +1,56 @@
 import React from 'react'
-
-
+import Logo from '../assets/MindMap.png'
+import styles from '../styles/Navbar.module.css'
 
 const navLinks = [
-    { title: 'Home', path: '/' },
-    { title: 'Explore Products', path: '/about' },
-    { title: 'Login', path: '/' },
+    {
+        name: 'Link',
+        url: '/',
+        icon: Logo,
+    },
+    {
+        name: 'Explore Product',
+        url: '/generation',
+        className: "",
+    },
+    {
+        name: 'Login',
+        url: '/login',
+        className: "",
+    }
 ]
 
-const Navbar = () => {
-  return (
-    <nav>
-        <div>
 
-        </div>
-    </nav>
+const Navbar = () => {
+    
+    return (
+        <nav className={`fixed top-0 w-full flex justify-between items-center py-4 pl-10 font-primary bg-white shadow-md`}>
+            {/* Left : Logo */}
+            <div className='flex space-x-4 font-bold text-3xl '>
+                <a href={navLinks[0].url}
+                    className='flex items-center '
+                >
+                {navLinks[0].icon && (
+                    <img src={navLinks[0].icon} alt='logo' 
+                        className='mr-2' />
+                )}
+                {navLinks[0].name}
+                </a>
+            </div>
+            
+            {/* Right : Navigation */}
+            <div className='space-x-8 mr-20'>
+                {navLinks.slice(1).map((link,index) => (
+                    <a key={index}
+                        href={link.url}
+                        className={`text-md ${link.className || ''}`}
+                    >
+                    {link.name}    
+                    </a>
+                ))}
+            </div>
+        </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
