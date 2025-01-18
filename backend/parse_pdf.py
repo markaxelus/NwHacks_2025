f3dccf9f2e88b5d6122d46749e28807590547064
Mark <mrkaxelus@gmail.com>
2025-01-18 13:35:33 -0800

    function testing & gitignore updates for generated dirs

--- backend/parse_pdf.py
+++ backend/parse_pdf.py
@@ -25,22 +25,53 @@ def extract_text(input_doc, output_dir) -> None:
     except Exception as e:
         print(f"Error extracting text from the pdf: {e}")
 
-def extract_images(input_doc, output_dir) -> None:
+def extract_images(input_file:str , output_image:str) -> None:      
+
     try:
-        input_doc = fitz.open(input_doc)
+        # Open the inputfile with fitz
+        input_doc = fitz.open(input_file)
 
         for page in range(len(input_doc)):
             cur_page = input_doc[page]
+            # Returns metadata of the images
             images = cur_page.get_images(full=True)
-            for img in images:
-                xref = image[0]
-                base_image = input_doc.extract_image(xref)
-                image_bytes = base_image["image"]
-                image = Image.open(image_bytes)
-                image_path = os.path.join(output_dir, f"page_{page+1}_image_{xref}.png")
-                image.save(image_path)
-                print(f"Extracted image from the pdf saved to: {image_path}")
+            for img_index, img in enumerate(images, start=1):
+                xref = img[0]
+                save_image(input_doc, xref, output_image, page + 1, img_index)
         input_doc.close()
+        print(f"Extracted images from the pdf saved to: {output_image}")
+
+    except Exception as e:
+        print(f"Extracting images failed from the pdf: {e}")
+
+def save_image(doc: fitz.Document, xref:int, output_dir_image:str, page_number:int, img_index:int) -> None:
+    """
+    """
+
+    base_img = doc.extract_image(xref)
+    img_bytes = base_img["image"]
+    img_ext = base_img["ext"]
+
+    # Define path
+    image_path = os.path.join(output_dir_image, f"page_{page_number}_image_{img_index}.{img_ext}")
+
+    # Save image
+    try:
+        with open(image_path, "wb") as image_file:
+            image_file.write(img_bytes)
+        
+        print(f"Saved image successfully: {image_path}")
     
     except Exception as e:
-        print(f"Error extracting images from the pdf: {e}")
+        print(f"Failed to save image: {e}")
+
+# For testing purposes, later not needed when integrated with FastAPI
+def main():
+    os.makedirs("output_txt", exist_ok=True)
+    os.makedirs("output_img", exist_ok=True)
+
+    extract_text("sample.pdf", "output_txt")
+    extract_images("sample.pdf", "output_img")
+
+if __name__ == "__main__":
+    main()
