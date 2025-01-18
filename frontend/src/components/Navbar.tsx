6ee36d5228b212708cce39483bd3926c752aa815
Mark <mrkaxelus@gmail.com>
2025-01-18 12:52:33 -0800

    Navbar setup

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 
+
+
+const navLinks = [
+    { title: 'Home', path: '/' },
+    { title: 'Explore Products', path: '/about' },
+    { title: 'Login', path: '/' },
+]
+
 const Navbar = () => {
   return (
-    <div>
+    <nav>
         <div>
-            
+
         </div>
-    </div>
+    </nav>
   )
 }
 
