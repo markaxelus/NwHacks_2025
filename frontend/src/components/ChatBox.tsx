c47450094a6b1bc73f3875393ab4d7d66fccf842
Mark <mrkaxelus@gmail.com>
2025-01-19 05:34:32 -0800

    regenerate button

--- frontend/src/components/ChatBox.tsx
+++ frontend/src/components/ChatBox.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { useDarkMode } from "../components/DarkMode";
+
+import Sparkling from "../assets/Sparkling.png";
+import DSparkling from "../assets/dark/DSparkling.png";
 
 const ChatBox = ({
   regenerate,
@@ -7,15 +11,20 @@ const ChatBox = ({
   regenerate: (fileName: string) => void;
   fileName: string; // Pass fileName as a prop
 }) => {
+
+  const { darkMode } = useDarkMode(); 
   return (
-    <div className="h-full w-full rounded-3xl flex flex-col items-center justify-between filter dark:invert">
+    <div className=" w-full rounded-3xl filter dark:invert">
       {/* Button Container */}
-      <div className="m-4 w-[100%] h-[90%] pl-8 flex items-center justify-right overflow-y-auto filter dark:invert ">
+      <div className="m-24 h-[40%] overflow-y-auto  filter dark:invert ">
         <button
           onClick={() => regenerate(fileName)} // Call regenerate with fileName
-          className="border-2 h-full w-1/5 border-white px-4 py-2 rounded hover:bg-white hover:text-gray-900 transition duration-300 ease-in-out"
+          className="border-2 flex justify-between items-center border-black dark:border-white w-[35%] px-12 py-2 rounded  hover:text-gray-500 transition duration-300 ease-in-out dark:bg-gradient-to-r 
+          dark:from-[#60DDD9] 
+          dark:to-[#347775]"
         >
           Regenerate
+          <img src={darkMode ? DSparkling : Sparkling} />
         </button>
       </div>
     </div>
