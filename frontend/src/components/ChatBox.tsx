fbe2fa06988fd2695d08034f8a4e18085dc9818e
Mark <mrkaxelus@gmail.com>
2025-01-19 03:22:20 -0800

    Huge button incoming

--- frontend/src/components/ChatBox.tsx
+++ frontend/src/components/ChatBox.tsx
@@ -4,7 +4,9 @@ const ChatBox = () => {
   return (
     <div className="h-full w-full pb-4 bg-gray-900/[0.1] p-4 rounded-3xl flex flex-col items-center justify-between filter dark:invert ">
       <div className="m-4 w-[95%] h-[70%] flex items-right justify-right overflow-y-auto filter dark:invert">
-        CHAT GOES HERE
+        <button className=" border-2 border-white ">
+          Create any button
+        </button>
       </div>
 
       <input
